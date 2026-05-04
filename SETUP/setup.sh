d5ee7f21e4c5d455237c6d262030aca5b547aa28
Alperen ERKAN <erkanalperen54@gmail.com>
2026-05-05 02:02:20 +0300

    Fix formatting in setup.sh for environment variable export

--- SETUP/setup.sh
+++ SETUP/setup.sh
@@ -1,5 +1,5 @@
 setup_tmp=`mktemp -d -t setup`
-printenv | sort > "${setup_tmp}/orig"
+printenv | sort > "${setup_tmp}/orig" 
 make print_exports | grep -E -v '^(MAKE|MFLAGS|SHLVL)' > "${setup_tmp}/exports"
 eval `comm -13 "${setup_tmp}/orig" "${setup_tmp}/exports" | sed 's,^\(.*\)$,export "\1",'`
 
