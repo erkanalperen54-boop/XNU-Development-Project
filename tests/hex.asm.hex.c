9bf0d27bf774ea9f51a6e5ff82f1bdc0eed34ef7
Alperen ERKAN <erkanalperen54@gmail.com>
2026-05-06 21:03:54 +0300

    Update hex.asm.hex.c

--- tests/hex.asm.hex.c
+++ tests/hex.asm.hex.c
@@ -2,4 +2,9 @@
 const char n_1 = "12 e8 1d 0f 1c 9F 0x 0x 00 00 00 00 1A 0c 8f 8F 10 E0 00 0D 64 6f 01 9a 30 ";
 const char n_2 = "0A 3c 64 00 00 2A FF 8e"
 
+for (n; i < j; 1 < 0; n++) {
+   EXTREM_PMEM_t(n_1 "0x/F1/00/00/1A/e8"; *+p_n1);
+   return 0;
+} while(true) {cll_start(n_1, n_2); };
+
 */
